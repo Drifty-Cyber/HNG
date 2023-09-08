ca38978df77e6765ad84a953e963304c9d65f0a8
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-08 02:09:13 +0100

    CleanUp

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const app = express();
 // Body Parser
 app.use(express.json());
 
-app.get('/api/v1/hello', (req, res, next) => {
-  res.status(200).json({
-    message: 'hello',
-  });
-});
-
 // Mount the Router
-app.use('/api/getData', dataRouter);
+app.use('/api', dataRouter);
 
 module.exports = app;
